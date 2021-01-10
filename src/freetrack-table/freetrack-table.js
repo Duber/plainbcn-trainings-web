efb990ca229c095a7d91c502d4c38915660c8e49
Miguel Cruz <miguel@miguelcruz.net>
2021-01-10 21:42:07 +0100

    freetrack likes

--- src/freetrack-table/freetrack-table.js
+++ src/freetrack-table/freetrack-table.js
@@ -33,40 +33,61 @@ const columns = [
         Header: 'Type',
         accessor: 'type',
         Filter: SelectColumnFilter,
-        width: "10vmax"
+        width: '10vmax'
     },
     {
         Header: 'Area',
         accessor: 'area',
         Filter: SelectColumnFilter,
-        width: "10vmax"
+        width: '10vmax'
     },
     {
         Header: 'Level',
         accessor: 'level',
         Filter: SelectColumnFilter,
-        width: "10vmax"
+        width: '10vmax'
     },
     {
         Header: 'Title',
         accessor: 'title',
         Filter: TextSearchColumnFilter,
-        width: "40vmax"
+        width: '40vmax'
     },
     {
         Header: 'Scheduled',
         accessor: 'scheduled',
         Filter: ScheduledColumnFilter,
         filter: ScheduledColumnFilterFn,
-        width: "10vmax"
+        width: '10vmax'
     },
     {
         Header: 'Likes',
         accessor: 'likes',
         disableFilters: true,
-        width: "5vmax"
+        width: '1vmax',
+        Cell: renderLikes
+    },
+    {
+        id: 'liked',
+        accessor: 'liked'
+    },
+    {
+        id: 'id',
+        accessor: 'id'
     }
 ]
+
+function renderLikes({ row, value }) {
+    let cellStyle = row.values.liked ? 'freetrack-likecol freetrack-likecol__liked' : 'freetrack-likecol freetrack-likecol__notliked'
+    let iconStyle = row.values.liked ? 'fas fa-heart' : 'far fa-heart'
+    return (
+        <button className={cellStyle} data-id={row.values.id} onClick={toggleLike}><i className={iconStyle} /> {value}</button>
+    )
+}
+
+function toggleLike(e) {
+    console.log(e.target.dataset.id)
+}
 
 function sortData(data) {
     return data.sort((a, b) => {
@@ -79,12 +100,16 @@ function sortData(data) {
     })
 }
 
-const initialState = { filters: [{ id: 'scheduled', value: 'true' }] }
+const initialState = {
+    filters: [{ id: 'scheduled', value: 'true' }],
+    hiddenColumns: ['liked', 'id']
+}
+
 
 export default class FreeTrackTable extends Component {
     render() {
         return (
-            <div className="freeTrackTable" >
+            <div className="freetrack-table" >
                 <Table columns={columns} data={sortData(this.props.data)} initialState={initialState} />
                 {this.props.data.length === 0 && <p>Loading ...</p>}
             </div>
