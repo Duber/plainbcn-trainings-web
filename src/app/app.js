c40411255c3e3f9cc091124cf972d01ae2344a1d
Miguel Cruz <miguel@miguelcruz.net>
2021-01-18 15:49:50 +0100

    cleanup unused imports in app

--- src/app/app.js
+++ src/app/app.js
@@ -2,9 +2,6 @@ import './app.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SkillTable from '../skill-table/skill-table'
 import FreeTrackTable from '../freetrack-table/freetrack-table'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from '../navbar/navbar'
 import NotFound from '../not-found/not-found'
